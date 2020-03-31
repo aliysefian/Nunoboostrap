5b37877c4a797e0ee5e6caf6b10049eea2d5a1e6
ali <aliysefian@gmail.com>
2020-03-31 17:53:16 +0430

    3-navbar-scroll slow

--- js/custom.js
+++ js/custom.js
@@ -23,3 +23,18 @@ $(document).ready(function () {
         }
     })
 });
+/* scroll smooth navigation  */
+$(document).ready(function () {
+    $('a').on('click',function (e) {
+        console.log(this.hash)
+        if(this.hash!==""){
+            e.preventDefault();
+            var hash=this.hash;
+            $('html','body').animate({
+                scrollTop:$(hash).offset().top
+            },800,function () {
+                window.location.hash=hash;
+            })
+        }
+    })
+});
